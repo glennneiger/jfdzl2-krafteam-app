ee7e1ddc76ae84957648935c1b66b54d833aa80d
Grzegorz Kolek <gkolek@gazeta.pl>
2019-02-10 21:35:07 +0100

    simple tile-list structure

--- src/routes/BarList/index.js
+++ src/routes/BarList/index.js
@@ -5,6 +5,14 @@ class BarList extends Component {
         return (
             <div>
                 BarList
+                <p>
+                    <span>Bar1  </span>
+                    <span>Bar2  </span>
+                </p>
+                <p>
+                    <span>Bar3  </span>
+                    <span>Bar4  </span>
+                </p>
             </div>
         );
     }
