da0d7d7862ac6048be78708a4eb89feb59a44319
Michalina Zeligowska <michalina.zeligowska@gmail.com>
2019-02-10 12:39:17 +0100

    material-ui fixed

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import NavBar from './components/NavBar';
+import BottomBar from './components/BottomBar';
+import Main from './routes/Main'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <div>
+                    <NavBar />
+
+                    <Route path='/' component={Main} />
+
+                    <BottomBar />
+                </div>
+            </Router>
+        );
+    }
+}
+
+export default App;
--- src/routes/Main/index.js
+++ src/routes/Main/index.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
-import NavBar from '../../components/NavBar';
-import BottomBar from '../../components/BottomBar';
 
 class Main extends Component {
   render() {
     return (
       <div>
-        <NavBar />
         Main
-        <BottomBar />
       </div>
     );
   }
