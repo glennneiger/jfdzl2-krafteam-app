c743c8d097f4c142dd111145d9930e0c1ff3dbcd
Kwadratowy <m.gorski@op.pl>
2019-02-17 08:07:12 +0100

    charts

--- src/routes/Rank/index.js
+++ src/routes/Rank/index.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
+import { Chart } from "react-charts";
 
-class Rank extends Component {
-    render() {
-        return (
-            <div>
-                TEST TEST TEST !!!!!!
-                
-            </div>
-        );
-    }
-}
+
+// class Rank extends Component {
+//     render() {
+//         return (
+           let data= [
+                [[1, 10], [2, 10], [3, 10]],
+                [[1, 10], [2, 10], [3, 10]],
+                [[1, 10], [2, 10], [3, 10]]
+              ];
+               
+              <div
+                style={{
+                  width: "400px",
+                  height: "300px"
+                }}
+              >
+                <Chart
+                  data={data}
+                  axes={[
+                    { primary: true, type: "linear", position: "bottom" },
+                    { type: "linear", position: "left" }
+                  ]}
+                />
+              </div>;
+//         );
+//    }
+// }
 
 export default Rank
