b1a3b16963f0bb4da58e0351f85d4009f33f4eb1
Robert Kwasnicki <rkwas005@wp.pl>
2019-02-17 12:03:32 +0100

    created beerlist link

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar';
 import BottomBar from './components/BottomBar';
 import Main from './routes/Main'
 import BarList from './routes/BarList'
+import BeerList from './routes/BeerList'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import 'normalize.css';
@@ -16,6 +17,7 @@ class App extends Component {
 
                     <Route exact path='/' component={Main} />
                     <Route path='/bar-list' component={BarList} />
+                    <Route path='/beer-list' component={BeerList} />
 
                     <BottomBar />
                 </div>
---
+++ src/routes/BeerList/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import NavBar from '../../components/NavBar';
+
+class BeerList extends Component {
+   render() {
+     return (
+       <div>
+           Beer1
+           Beer2
+           Beer3
+           Beer4
+           Beer5
+           Beer6
+       </div>
+     );
+   }
+ }
+
+ export default BeerList;
