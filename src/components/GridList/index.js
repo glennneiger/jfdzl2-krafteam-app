5645b7c79c3f2c039e4975261093ca1eb3315e5d
Grzegorz Kolek <gkolek@gazeta.pl>
2019-02-17 13:09:04 +0100

    main gridlist

--- src/components/GridList/index.js
+++ src/components/GridList/index.js
@@ -7,7 +7,14 @@ import GridListTileBar from '@material-ui/core/GridListTileBar';
 import ListSubheader from '@material-ui/core/ListSubheader';
 import IconButton from '@material-ui/core/IconButton';
 import InfoIcon from '@material-ui/icons/Info';
-import tileData from './tileData';
+// import tileData from './tileData';
+
+import bar01 from '../Tiles/daffyduck.png'
+import bar02 from '../Tiles/bugsbunny.png'
+import bar03 from '../Tiles/coyote.png'
+import bar04 from '../Tiles/roadrunner.png'
+import bar05 from '../Tiles/Brick.jpg'
+import bar06 from '../Tiles/hop.jpg'
 
 const styles = theme => ({
   root: {
@@ -18,14 +25,22 @@ const styles = theme => ({
     backgroundColor: theme.palette.background.paper,
   },
   gridList: {
-    width: 500,
-    height: 450,
+    width: '60%',
+    height: 600
   },
   icon: {
     color: 'rgba(255, 255, 255, 0.54)',
   },
 });
 
+const tileData = [
+  {name: 'bar 01', location: 'lublin 111', image: bar01, title: 'daffyduck'},
+  {name: 'bar 02', location: 'lublin 222', image: bar02, title: 'bugsbunny'},
+  {name: 'bar 03', location: 'lublin 333', image: bar03, title: 'coyote'},
+  {name: 'bar 04', location: 'lublin 444', image: bar04, title: 'roadrunner'},
+  {name: 'bar 05', location: 'lublin 555', image: bar05, title: 'roadrunner'},
+  {name: 'bar 06', location: 'lublin 666', image: bar06, title: 'hop'},
+];
 /**
  * The example data is structured as follows:
  *
@@ -48,16 +63,16 @@ function TitlebarGridList(props) {
 
   return (
     <div className={classes.root}>
-      <GridList cellHeight={180} className={classes.gridList}>
+          <ListSubheader component="div">BarList</ListSubheader>
+      <GridList cellHeight={400} className={classes.gridList}>
         <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
-          <ListSubheader component="div">December</ListSubheader>
         </GridListTile>
         {tileData.map(tile => (
           <GridListTile key={tile.img}>
-            <img src={tile.img} alt={tile.title} />
+            <img src={tile.image} alt={tile.title} />
             <GridListTileBar
-              title={tile.title}
-              subtitle={<span>by: {tile.author}</span>}
+              title={tile.name}
+              subtitle={<span>address: {tile.location}</span>}
               actionIcon={
                 <IconButton className={classes.icon}>
                   <InfoIcon />
--- src/routes/BarList/index.js
+++ src/routes/BarList/index.js
@@ -5,16 +5,7 @@ class BarList extends Component {
     render() {
         return (
             <div>
-                BarList
-                <p>
-                    <span>Bar1  </span>
-                    <span>Bar2  </span>
-                </p>
-                <p>
-                    <span>Bar3  </span>
-                    <span>Bar4  </span>
-                </p>
-<GridList></GridList>
+                <GridList/>
             </div>
         );
     }
