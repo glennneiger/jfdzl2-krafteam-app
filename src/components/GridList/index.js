c2906dbdff96c5dda3629cf0eddacd431e0b92a2
Grzegorz Kolek <gkolek@gazeta.pl>
2019-02-18 18:01:57 +0100

    palette-primary applied

--- src/components/GridList/index.js
+++ src/components/GridList/index.js
@@ -13,7 +13,7 @@ import bar01 from '../Tiles/daffyduck.png'
 import bar02 from '../Tiles/bugsbunny.png'
 import bar03 from '../Tiles/coyote.png'
 import bar04 from '../Tiles/roadrunner.png'
-import bar05 from '../Tiles/Brick.jpg'
+import bar05 from '../Tiles/brick.jpg'
 import bar06 from '../Tiles/hop.jpg'
 
 const styles = theme => ({
@@ -25,21 +25,27 @@ const styles = theme => ({
     backgroundColor: theme.palette.background.paper,
   },
   gridList: {
-    width: '60%',
+    width: 800,
     height: 600
   },
   icon: {
     color: 'rgba(255, 255, 255, 0.54)',
   },
+  palettePrimary: {
+    backgroundColor: '#212529',
+    color: '#fed136',
+    border: '#fed136 5px solid',
+    borderRadius: 5
+  }
 });
 
 const tileData = [
-  {name: 'bar 01', location: 'lublin 111', image: bar01, title: 'daffyduck'},
-  {name: 'bar 02', location: 'lublin 222', image: bar02, title: 'bugsbunny'},
-  {name: 'bar 03', location: 'lublin 333', image: bar03, title: 'coyote'},
-  {name: 'bar 04', location: 'lublin 444', image: bar04, title: 'roadrunner'},
-  {name: 'bar 05', location: 'lublin 555', image: bar05, title: 'roadrunner'},
-  {name: 'bar 06', location: 'lublin 666', image: bar06, title: 'hop'},
+  { name: 'bar 01', location: 'lublin 111', image: bar01, title: 'daffyduck' },
+  { name: 'bar 02', location: 'lublin 222', image: bar02, title: 'bugsbunny' },
+  { name: 'bar 03', location: 'lublin 333', image: bar03, title: 'coyote' },
+  { name: 'bar 04', location: 'lublin 444', image: bar04, title: 'roadrunner' },
+  { name: 'bar 05', location: 'lublin 555', image: bar05, title: 'roadrunner' },
+  { name: 'bar 06', location: 'lublin 666', image: bar06, title: 'hop' },
 ];
 /**
  * The example data is structured as follows:
@@ -63,7 +69,7 @@ function TitlebarGridList(props) {
 
   return (
     <div className={classes.root}>
-          <ListSubheader component="div">BarList</ListSubheader>
+      <ListSubheader component="div">BarList</ListSubheader>
       <GridList cellHeight={400} className={classes.gridList}>
         <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
         </GridListTile>
@@ -71,7 +77,8 @@ function TitlebarGridList(props) {
           <GridListTile key={tile.img}>
             <img src={tile.image} alt={tile.title} />
             <GridListTileBar
-              title={tile.name}
+               className={classes.palettePrimary}
+               title={tile.name}
               subtitle={<span>address: {tile.location}</span>}
               actionIcon={
                 <IconButton className={classes.icon}>
