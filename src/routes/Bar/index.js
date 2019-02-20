71497109291201b314123984247dc75f111372f2
Grzegorz Kolek <gkolek@gazeta.pl>
2019-02-20 02:22:56 +0100

    main BarCard view

--- src/routes/Bar/index.js
+++ src/routes/Bar/index.js
@@ -1,13 +1,143 @@
 import React, { Component } from 'react';
 
-class Bar extends Component {
+import GridList from '../../components/GridList'
+// class Bar extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 Karta wybranego baru
+//             </div>
+//         );
+//     }
+// }
+
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import classnames from 'classnames';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import red from '@material-ui/core/colors/red';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import StarIcon from '@material-ui/icons/Star';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+
+const styles = theme => ({
+    card: {
+        maxWidth: 800,
+    },
+    media: {
+        height: 0,
+        paddingTop: '56.25%', // 16:9
+    },
+    actions: {
+        display: 'flex',
+    },
+    expand: {
+        transform: 'rotate(0deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest,
+        }),
+    },
+    expandOpen: {
+        transform: 'rotate(180deg)',
+    },
+    avatar: {
+        backgroundColor: '#fed136',
+    },
+});
+
+class BarCard extends React.Component {
+    state = { expanded: false };
+
+    handleExpandClick = () => {
+        this.setState(state => ({ expanded: !state.expanded }));
+    };
+
     render() {
+        const { classes } = this.props;
+
         return (
-            <div>
-                Karta wybranego baru
-            </div>
+            <Card className={classes.card}>
+                <CardHeader
+                    avatar={
+                        <Avatar
+                            aria-label="Recipe"
+                            className={classes.avatar}
+                            style={{ color: '#212529' }}>
+                            03
+                        </Avatar>
+                    }
+                    action={
+                        <IconButton>
+                            <MoreVertIcon />
+                        </IconButton>
+                    }
+                    title="BAR 03"
+                    subheader={<span>address: lublin 333</span>}
+                    image='../../components/panties.jpg'
+                />
+                <CardMedia
+                    className={classes.media}
+                    image="/static/images/cards/paella.jpg"
+                    title="BAR 03"
+                />
+                <CardContent>
+                    <Typography component="p">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras malesuada bibendum nibh ac rutrum. In ullamcorper ullamcorper augue, ac imperdiet nulla dignissim in. Integer et sodales nibh. Proin tincidunt nunc ac turpis lacinia, et placerat massa cursus.
+          </Typography>
+                </CardContent>
+                <CardActions className={classes.actions} disableActionSpacing>
+                    <IconButton aria-label="Add to favorites"style={{ color: 'red' }}>
+                        <FavoriteIcon />
+                    </IconButton>
+                    <IconButton aria-label="Share">
+                        <ShareIcon />
+                    </IconButton>
+                    <IconButton aria-label="Rate"  style={{ color: '#fed136' }}>
+                        <StarIcon className={classes.rate} />
+                        <StarIcon className={classes.rate} />
+                        <StarBorderIcon className={classes.rate} />
+                        <StarBorderIcon className={classes.rate} />
+                        <StarBorderIcon className={classes.rate} />
+                    </IconButton>
+
+                    <IconButton
+                        className={classnames(classes.expand, {
+                            [classes.expandOpen]: this.state.expanded,
+                        })}
+                        onClick={this.handleExpandClick}
+                        aria-expanded={this.state.expanded}
+                        aria-label="Show more"
+                    >
+                        <ExpandMoreIcon />
+                    </IconButton>
+                </CardActions>
+                <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
+                    <CardContent>
+                        <Typography paragraph>Method:</Typography>
+                        <Typography paragraph>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras malesuada bibendum nibh ac rutrum. In ullamcorper ullamcorper augue, ac imperdiet nulla dignissim in. Integer et sodales nibh. Proin tincidunt nunc ac turpis lacinia, et placerat massa cursus. Vivamus efficitur urna et neque dignissim tincidunt. Fusce at libero quis nibh condimentum volutpat a sed arcu. Curabitur massa nulla, mattis quis pulvinar vitae, egestas non arcu. Curabitur nec lorem eu tellus feugiat aliquet.
+            </Typography>
+                    </CardContent>
+                </Collapse>
+            </Card>
         );
     }
 }
 
-export default Bar;
+BarCard.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(BarCard);
